abbb86b63a3b480a2cf4f218716a2ebcebc7f78d
Michael Skinnider <michaelskinnider@gmail.com>
2017-10-24 15:51:34 -0700

    Fix bug in filter_breadth (different column names in GAF/GPA)

--- R/filter_breadth.R
+++ R/filter_breadth.R
@@ -14,10 +14,13 @@ filter_breadth <- function(goa, min.breadth = 20, max.breadth = 1000) {
   message("filtering GOA data for terms with minimum breadth ", min.breadth, 
           " and maximum breadth ", max.breadth, "...")
   # calculate GO term frequency
-  freq <- table(goa$GO.ID)
+  if (!"GO.ID" %in% colnames(goa) & !"GO_ID" %in% colnames(goa))
+    stop("Couldn't find GO term column (expecting one of: GO.ID, GO_ID)")
+  colname <- ifelse("GO.ID" %in% colnames(goa), "GO.ID", "GO_ID")
+  freq <- table(goa[[colname]])
   # exclude narrow or broad terms
   freq <- freq[freq >= min.breadth & freq <= max.breadth]
   # now filter the GOA
-  goa <- goa[goa$GO.ID %in% names(freq),]
+  goa <- goa[goa[[colname]] %in% names(freq),]
   return(goa)  
 }
