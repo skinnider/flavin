b85793e8a739e99d174954a8fbbe58bca717b431
Michael Skinnider <michaelskinnider@gmail.com>
2017-11-08 13:51:33 -0800

    Don't reorder input nodes in network_shared_annotations

--- R/network_shared_annotations.R
+++ R/network_shared_annotations.R
@@ -1,14 +1,19 @@
+#' Calculate shared annotations between network nodes
+#' 
 #' Calculate the number of shared annotations between each pair of genes or 
-#' proteins in a network. 
+#' proteins in a network, and append this value to the input data frame
+#' as an extra column. 
 #' 
 #' @param network a data.frame with nodes (genes) in the first two columns
 #' @param shared a matrix containing the number of shared annotations, e.g. 
 #' GO terms, between each pair of proteins (as output by `shared_annotations`)
-#' @param keep.all if false, only results for network edges where both nodes
-#' were found in the shared annotations matrix will be reported 
+#' @param col_name the name of the new column to create; defaults to "coexpr"
+#' 
 #' @return the input network, with an extra column containing the number of 
 #' shared annotations (e.g., GO terms)
+#' 
 #' @export
+#'
 #' @examples 
 #' # read network
 #' net <- read.delim("mouse-network.tsv.gz")
@@ -16,20 +21,18 @@
 #' shared <- shared_annotations(ann)
 #' # calculate number of shared GO terms for each edge the network
 #' net.shared <- network_shared_go(net, shared)
-network_shared_annotations <- function(network, shared, keep.all = F) {
+network_shared_annotations <- function(network, shared, col_name = "shared") {
+  # make sure there is some overlap between network and expression data
   inSharedIndices <- network[,1] %in% rownames(shared) & 
     network[,2] %in% rownames(shared)
   if (sum(inSharedIndices) == 0)
     stop("no nodes in the network were found in the shared annotations matrix")
+  # create empty column
+  network[[col_name]] <- NA
+  # add values for nodes in shared matrix
   in.shared <- network[inSharedIndices,]
   rowIndices <- match(in.shared[,1], rownames(shared))
   colIndices <- match(in.shared[,2], colnames(shared))
-  in.shared$shared <- shared[cbind(rowIndices, colIndices)]
-  if (keep.all) {
-    not.in.shared <- network[!inSharedIndices,]
-    not.in.shared$shared <- 0
-    return(rbind(in.shared, not.in.shared))
-  } else {
-    return(in.shared)  
-  }
+  network[[col_name]][inSharedIndices] <- shared[cbind(rowIndices, colIndices)]
+  return(network)
 }
